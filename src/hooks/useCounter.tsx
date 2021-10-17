b56fdd530c6775d19bcd1aff968dcbf5c70cdb04
Fernando Mare <fernando.mare03@gmail.com>
2021-10-17 17:12:28 -0300

    Subiendo Curso de React Native

---
+++ src/hooks/useCounter.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+
+export const useCounter = (inicial: number=10) => {
+    
+    const [valor, setvalor] = useState(inicial);
+
+    const acumular = (numero:number) => {
+        setvalor(valor+numero);
+    }
+
+
+    return{
+        valor,
+        acumular
+    }
+}
